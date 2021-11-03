d4c5ed7700ccd860a28113e534d6652c5de763e7
Kendra Chateau <kendra@super.gd>
2021-11-03 08:52:16 -0700

    Fix type in 'has_and_belongs_to_many'

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   has_and_belongs_to_many :authors
-  has_and_belong_to_many :categories
+  has_and_belongs_to_many :categories
   has_many :authors
   has_many :user_reads
 end
