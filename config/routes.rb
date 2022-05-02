e06570f5a31634a476cd5a4fe6a62be4bd76184b
Kendra Chateau <kendra@super.gd>
2022-05-02 16:13:38 -0700

    Update root route to summary page

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root :to => redirect('/books/shelf')
+  root :to => redirect('/books/summary')
 
   resource :books do
     get "shelf"
+    get "summary"
     get "search"
     get "more_info"
     post "search_results"
